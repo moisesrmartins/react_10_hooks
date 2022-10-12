c35811a7002b1c3aab18bc46c8855cdce9001557
Moisés R. Martins <90099815+moisesrmartins@users.noreply.github.com>
2022-10-12 10:51:22 -0300

    React_10_Hooks Parte 2.2: useEffect e useReducer

--- custom_hooks/src/Hooks.js
+++ custom_hooks/src/Hooks.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useEffect, useReducer } from "react";
+
+const reducer = (state, action) => {
+  console.log("state", state, "action", action);
+};
 
 function Hooks() {
+  const [data, dispatch] = useReducer(reducer);
+
+  useEffect(() => {
+    dispatch({ type: "REQUEST" });
+  }, []);
+
   return (
     <div className="Hooks">
       <h1>Custom-Hooks</h1>
+      {JSON.stringify(data)}
     </div>
   );
 }
