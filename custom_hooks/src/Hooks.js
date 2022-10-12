3e399413e938471a6493943a8715fd533153078e
Moisés R. Martins <90099815+moisesrmartins@users.noreply.github.com>
2022-10-12 15:51:32 -0300

    React_10_Hooks Parte 3: useGet

--- custom_hooks/src/Hooks.js
+++ custom_hooks/src/Hooks.js
@@ -1,31 +1,11 @@
-import React, { useEffect, useReducer } from "react";
-import axios from "axios";
+import React from "react";
+import useGet from "./useGet";
 
 const url = "https://hookproject-a7d09-default-rtdb.firebaseio.com/hooks.json";
 
-const reducer = (state, action) => {
-  console.log("state", state, "action", action);
-  if (action.type === "REQUEST") {
-    return {
-      ...state,
-      loading: true,
-    };
-  }
-  if (action.type === "SUCCESS") {
-    return { ...state, loading: false, data: action.data };
-  }
-  return state;
-};
-
 function Hooks() {
-  const [data, dispatch] = useReducer(reducer, { loading: true, data: {} });
-
-  useEffect(() => {
-    dispatch({ type: "REQUEST" });
-    axios.get(url).then((res) => {
-      dispatch({ type: "SUCCESS", data: res.data });
-    });
-  }, []);
+  const data = useGet(url);
+  const data2 = useGet("https://httpbin.org/ip");
 
   if (data.loading) {
     return <p>Loading...</p>;
@@ -35,6 +15,7 @@ function Hooks() {
     <div className="Hooks">
       <h1>Custom-Hooks</h1>
       {JSON.stringify(data)}
+      <pre>{JSON.stringify(data2)}</pre>
     </div>
   );
 }
