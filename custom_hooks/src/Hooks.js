1f101ebe0cfebf87afa9797303bff3906e51f9d5
Moisés R. Martins <90099815+moisesrmartins@users.noreply.github.com>
2022-10-13 11:38:23 -0300

    React_10_Hooks Parte 4.4: Post e saveNew

--- custom_hooks/src/Hooks.js
+++ custom_hooks/src/Hooks.js
@@ -7,6 +7,11 @@ const url = "https://hookproject-a7d09-default-rtdb.firebaseio.com/hooks.json";
 function Hooks() {
   const data = useGet(url);
   const data2 = useGet("https://httpbin.org/ip");
+  const [postData, post] = usePost(url);
+
+  const saveNew = () => {
+    post({ chave: "value", key: "value1", valor: 10 });
+  };
 
   if (data.loading) {
     return <p>Loading...</p>;
@@ -17,6 +22,8 @@ function Hooks() {
       <h1>Custom-Hooks</h1>
       {JSON.stringify(data)}
       <pre>{JSON.stringify(data2)}</pre>
+      <button onClick={saveNew}>Save</button>
+      <pre>{JSON.stringify(postData)}</pre>
     </div>
   );
 }
